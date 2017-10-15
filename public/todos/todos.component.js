75a6e5811fcc66a0bd4fae74bdcd6f79435e6329
Kevin Erickson <kevin.erick01@gmail.com>
2017-10-15 11:41:49 -0600

    tagline animation timer

--- public/todos/todos.component.js
+++ public/todos/todos.component.js
@@ -6,12 +6,22 @@
             templateUrl: '/todos/todos.template.html'
         });
 
-        homeController.$inject = ['$scope', 'todoService'];
-        function homeController($scope, todoService) {
-
+        homeController.$inject = ['$scope','$interval', '$timeout', 'todoService'];
+        function homeController($scope, $interval, $timeout, todoService) {
             const vm = this;
-
+            
             vm.$onInit = function() {
+                vm.tagline = '';
+                const changeTag = '...do it now!';
+                let i = 0;
+                $interval(() => {
+                    vm.tagline += changeTag[i];
+                    ++i;
+                }, 400, 13, true);
+                
+                    
+                
+
                 todoService.getTodos().then((response) => {
                     vm.todos = response.data;
                 })
@@ -19,12 +29,17 @@
 
             vm.onAddTodo = function(e) {
                 e.preventDefault();
-                todoService.addTodo(vm.todo)
-                todoService.getTodos().then((response) => {
-                    vm.todos = response.data;
-                }).then(() => {
-                    delete vm.todo.name;
-                })
+                if (vm.todo) {
+                    todoService.addTodo(vm.todo);
+                    todoService.getTodos().then((response) => {
+                        vm.todos = response.data;
+                    }).then(() => {
+                        delete vm.todo.name;
+                    })
+                } else {
+                    vm.valid = false;
+                }
+                
                 
             }
 
@@ -39,5 +54,3 @@
 
 }())
 
-// funny video
-// https://www.youtube.com/watch?v=6x31lI8JdXY
